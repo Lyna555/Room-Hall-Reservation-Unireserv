1b93472fc65669a042aa489339312585f48b2f61
Lyna555 <bouglina3@gmail.com>
2022-04-12 06:34:11 +0100

    commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,13 +1,12 @@
 <x-app-layout>
     @include('admin.navigation-menu')
     @if(session()->has('error'))
-        <div class="alert alert-danger">
-            {{session()->get('error')}}
-        </div>
-  @endif
-<div style="display: flex;width:100%;height:90vh;justify-content:left;align-items:center">
-<img src="{{URL('images/image2.png')}}" style="height:80vh;width:50%"  alt="">
-
-</div>
+    <div class="alert alert-danger">
+        {{session()->get('error')}}
+    </div>
+    @endif
+    <div style="display: flex;width:100%;height:90vh;justify-content:left;align-items:center">
+        <img src="{{URL('images/image2.png')}}" style="height:80vh;width:50%" alt="">
+    </div>
 
 </x-app-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
     @include('navigation-menu')
-    <img src="{{URL('images/image1.png')}}"  style="height:80vh;width:50%" alt="">
+    <div style="display: flex;width:100%;height:90vh;justify-content:left;align-items:center">
+        <img src="{{URL('images/image2.png')}}" style="height:80vh;width:50%" alt="">
+    </div>
 </x-app-layout>
