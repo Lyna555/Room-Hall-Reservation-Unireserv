f640ff32db5cb99b0589d55f94a0257d1701a3e4
Lyna555 <bouglina3@gmail.com>
2022-04-28 12:41:58 +0100

    commit

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="height: 93.9vh;">
 
-        <div class=" w-full mt-6 px-6 py-4 shadow-md" style="width:30%;background-color: rgba(255, 255, 255, 0.452); border-radius:30px;">
+        <div class="card-30 mt-6 px-6 py-4 shadow-md" style="background-color: rgba(255, 255, 255, 0.452); border-radius:30px;">
             <div class="flex flex-col sm:justify-center items-center"> {{ $logo }}</div>
             {{ $slot }}
         </div>
