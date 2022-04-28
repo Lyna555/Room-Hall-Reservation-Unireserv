8858709082f28a3660cb0bad75fa310bf5e0974f
Lyna555 <bouglina3@gmail.com>
2022-04-28 21:14:33 +0100

    ccommit

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="height: 93.9vh;">
+    <div class="flex flex-col justify-center items-center pt-6 sm:pt-0" style="height: 93.9vh;">
 
         <div class="card-30 mt-6 px-6 py-4 shadow-md" style="background-color: rgba(255, 255, 255, 0.452); border-radius:30px;">
             <div class="flex flex-col sm:justify-center items-center"> {{ $logo }}</div>
